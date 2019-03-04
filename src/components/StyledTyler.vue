e33f114bbebe3a7f7b1ea1b75690a56a765c98fa
Adam Whitlock <adam.whitlock627@gmail.com>
2019-03-04 12:13:07 -0700

    dynnamic styling and removed tylers img

--- src/components/StyledTyler.vue
+++ src/components/StyledTyler.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="hello">
+    <p>
+      Below is a styled button component. You may pass a valid 'color' prop in
+      for text color, 'br' prop for border radius, 'bgc' prop for background
+      color, and 'pad' prop for padding.
+    </p>
     <styled-button color="orange" br="10px" pad="20px" bgc="brown"
       >I am a styled button.</styled-button
     >
-    <h3>Data inside of a string <br />item.url: {{ item.url }}</h3>
-    <a :href="item.url">Data bound to href attribute</a>
-    <h5>Data bound to image src attribute</h5>
+
     <hr />
-    <img :src="item.url" /> <img :src="item.tyler" />
-    <hr />
-    <p>
-      Want to change the data?<br />Use v-model to bind an input to some
-      data.<br />(try changing 300 to 310)
-    </p>
-    <input type="text" v-model="item.url" />
+
+    <styled-button
+      :color="dynColor"
+      :br="dynBr + 'px'"
+      :pad="dynPad + 'px'"
+      :bgc="dynBgc"
+      >I am a dynamically styled button. Change the fields below to change
+      me!</styled-button
+    >
+
+    <br /><span>Text Color </span><input type="color" v-model="dynColor" />
+    <br /><span>Background Color </span><input type="color" v-model="dynBgc" />
+    <br /><span>Padding (will be set in px) </span
+    ><input type="number" v-model="dynPad" /> <br /><span
+      >Border Radius (will be set in px) </span
+    ><input type="number" v-model="dynBr" />
   </div>
 </template>
 
 <script>
 import { StyledButton } from "../styled-components/StyledButton.js";
-import tylerImage from "@/assets/tyler.png";
 
 export default {
   name: "StyledTyler",
@@ -28,10 +39,10 @@ export default {
   },
   data() {
     return {
-      item: {
-        url: "https://www.placecage.com/gif/200/300",
-        tyler: tylerImage
-      }
+      dynColor: "#ffffff",
+      dynBr: 10,
+      dynPad: 15,
+      dynBgc: "#4ABB00"
     };
   }
 };
@@ -42,5 +53,6 @@ export default {
 input {
   width: 50%;
   text-align: center;
+  margin-top: 20px;
 }
 </style>
